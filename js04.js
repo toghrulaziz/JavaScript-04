5fa2b8a8bc6b4ec2214936188a6e7664e1a20b9a
toghrulaziz <togrul1609@gmail.com>
2023-10-04 16:07:19 +0400

    JavaScript 04

---
+++ js04.js
@@ -0,0 +1,33 @@
+div1.onmouseenter = ()=>{
+    div4.style.backgroundColor = 'darkgreen'
+}
+
+
+div1.onmouseleave = ()=>{
+    div4.style.backgroundColor = 'gray'
+}
+
+
+div2.onmouseenter = ()=>{
+    div4.style.backgroundColor = 'red'
+}
+
+
+div2.onmouseleave = ()=>{
+    div4.style.backgroundColor = 'gray'
+}
+
+
+
+div3.onmouseenter = ()=>{
+    div4.style.backgroundColor = 'blue'
+}
+
+
+div3.onmouseleave = ()=>{
+    div4.style.backgroundColor = 'gray'
+}
+
+
+
+
